fc3bf39d144816d982da9dfdb2795b30a2c97026
nganesh <nganesh@iu.edu>
2025-05-18 12:45:20 -0700

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+fruit = "watermelon"
+amount = 7
+price = 5.00
+y = False
+
+amount = float(amount)
+print("The data type of amount is", type(amount))
+
+price = int(price)
+print("The data type of price is", type(price))
+
+y = str(y)
+print("The data type of y is", type(y))
